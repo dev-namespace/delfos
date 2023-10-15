025287db4d35a9fa0e9df4cc4a23be7d37eadda2
Antonio Esquembre <antonioesquembre@Antonios-MacBook-Air.local>
2023-10-15 09:15:46 +0200

    made secrets optional

--- delfos-backend.el
+++ delfos-backend.el
@@ -13,13 +13,20 @@
 
 (require 'cl-lib)
 (require 'secrets)
+(require 'f)
+
+(defvar rc-path (expand-file-name ".delfosrc" user-emacs-directory))
+
+(make-directory user-emacs-directory t)
 
 (cl-defstruct delfos-backend complete-chat extract-answer models default-model)
 
 (defun delfos-backend-prompt-secret (label &optional prompt)
   (let ((api-key (read-from-minibuffer (or prompt (format "Enter secret (%s): " label)))))
-    (secrets-delete-item "login" (format "delfos-%s" label))
-    (secrets-create-item "login" (format "delfos-%s" label) api-key)
+    (condition-case _err
+        (secrets-delete-item "login" (format "delfos-%s" label))
+      (secrets-create-item "login" (format "delfos-%s" label) api-key)
+      (error (with-temp-file rc-path (insert api-key))))
     api-key))
 
 (defun delfos-backend-get-or-prompt-secret (label &optional prompt)
@@ -29,7 +36,11 @@
       (delfos-backend-prompt-secret label prompt))))
 
 (defun delfos-backend-get-secret (label)
-  (secrets-get-secret "login" (format "delfos-%s" label)))
+  (condition-case _err
+      (secrets-get-secret "login" (format "delfos-%s" label))
+    (error  (condition-case _err (f-read-text rc-path)
+              (error nil)))))
+
 
 (provide 'delfos-backend)
 ;;; delfos-backend.el ends here
